80cdb6f1c28b9de9f609fa224a0f9e83dff051fb
Dmitry-Alexeenko <dmitry.kniaz@yandex.ru>
2019-10-25 14:44:18 +0300

    Обновил create-react-app

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -4,14 +4,15 @@ class ProfileStatus extends React.Component {
     state = {
         editMode: false
     };
-    activateEditMode () {
+    activateEditMode =  () => {
+        console.log("this:", this);
         this.setState({ //React.Component возьмет этот объект и склеит его с первоначальным state
             editMode: true
         }); //берется из React.Component и перезаписывает элементы которые указаны выше в state
        /* this.forceUpdate(); //для того что бы перерисовать локальный state. Но лучше не использовать*/
     };
 
-    deactivateEditMode () {
+    deactivateEditMode  =  () => {
         this.setState({ //React.Component возьмет этот объект и склеит его с первоначальным state
             editMode: false
         }); //берется из React.Component и перезаписывает элементы которые указаны выше в state
@@ -23,10 +24,10 @@ class ProfileStatus extends React.Component {
             <div>
                 {!this.state.editMode
                     ? <div>
-                        <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span> {/*надо следать .bind(this) иначе this будет ссылаться на onDoubleClick*/}
+                        <span onDoubleClick={this.activateEditMode}>{this.props.status}</span> {/*надо следать .bind(this) иначе this будет ссылаться на onDoubleClick*/}
                     </div>
                     : <div>
-                        <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/> {/*onBlur работает на фокус*/}
+                        <input autoFocus={true} onBlur={this.deactivateEditMode} value={this.props.status}/> {/*onBlur работает на фокус*/}
                     </div>
                 }
             </div>
